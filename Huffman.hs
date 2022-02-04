7d6aea6032f390b32cec4ea107d18e86657ab514
Vincent Andersson <vinc.anders@gmail.com>
2022-02-04 10:40:41 +0100

    HuffmanTree Maybe? done

--- Huffman.hs
+++ Huffman.hs
@@ -3,9 +3,10 @@
 module Huffman(HuffmanTree, characterCounts, huffmanTree, codeTable, encode, compress, decompress) where
 
 import Table
-import PriorityQueue
+import PriorityQueue as PQ
 
 import Test.HUnit
+import BinomialHeap (isEmpty)
 
 {- a bit code (of a character or string) is represented by a list of Booleans
    INVARIANT:
@@ -16,6 +17,9 @@ type BitCode = [Bool]
 -- END OF DO NOT MODIFY ZONE
 
 --------------------------------------------------------------------------------
+
+pqEmpty :: PriorityQueue a
+pqEmpty = PQ.empty
 
 {- characterCounts s
    PRECONS: A string s.
@@ -29,7 +33,7 @@ characterCounts t = characterCountsAux t Table.empty
 {- characterCountsAux s t
    PRECONS: A string s and an empty table t.
    RETURNS: A table that maps each character that occurs in s to the number of 
-            times the character occurs in s.
+            times the character occurs in s. 
    EXAMPLE: 
    VARIANT: length s
 -}
@@ -52,11 +56,11 @@ frJust Nothing = error "Nothing in frJust."
 -- Snyggt! 
 --omvandlingen från char int i table till queue av träd krävde att vi skapar en aux som skapar leafs och insertar
 tableToQueue :: Table Char Int -> PriorityQueue HuffmanTree
-tableToQueue t = Table.iterate t tableToQueueAux PriorityQueue.empty
+tableToQueue t = Table.iterate t tableToQueueAux PQ.empty
 
 --
 tableToQueueAux :: PriorityQueue HuffmanTree -> (Char, Int) -> PriorityQueue HuffmanTree
-tableToQueueAux x y = PriorityQueue.insert x (createLeaf y)
+tableToQueueAux x y = PQ.insert x (createLeaf y)
 
 --skapar leafs och av key - value par, och lägger sedan till i par med leaf o value så att de kan insertas i queue
 createLeaf :: (Char, Int) -> (HuffmanTree, Int)
@@ -65,30 +69,53 @@ createLeaf (x,y) = ((Leaf x y),y)
 -- modify and add comments as needed
 data HuffmanTree = Leaf Char Int | Node Int HuffmanTree HuffmanTree deriving Show
 
-
-
 {- huffmanTree t
    PRECONS:  t maps each key to a positive value
    RETURNS: a Huffman tree based on the character counts in t
    EXAMPLE:
+   ! UNTESTED !
  -}
 huffmanTree :: Table Char Int -> HuffmanTree
-huffmanTree = undefined
+huffmanTree t = fst (fst (least (huffmanTreeAux (tableToQueue t))))
 
--- queue av träd buntas två och två tills att ett träd återstår,
-huffmanTreeAux :: PriorityQueue HuffmanTree -> HuffmanTree
-huffmanTreeAux = undefined
+{- huffmanTreeAux q
+   PRECONS: A priorityQueue of at least one HuffmanTree.
+   RETURNS: A combined Huffmantree from the HuffmanTrees in the priorityQueue
+   EXAMPLE: 
+   VARIANT: length PirorityQueue
+   ! UNTESTED !
+-}
+huffmanTreeAux :: PriorityQueue HuffmanTree -> PriorityQueue HuffmanTree
+huffmanTreeAux q
+  | PQ.is_empty q    = error "Empty Queue"
+  | PQ.is_empty nxtQ = q
+  | otherwise        = huffmanTreeAux (PQ.insert (snd (least nxtQ)) (mergeTrees nxtT nxt2T, prio + prio2))
+--     (The queue without the next two elements in queue) /\            /\ next two trees merged /\ the sum of respective priorities.
+  where nxtQ  = snd (least q)
+        nxtT  = fst (fst (least q))
+        prio  = snd (fst (least q))
+        nxt2Q = snd (least nxtQ)
+        nxt2T = fst (fst (least nxtQ))
+        prio2 = snd (fst (least nxtQ))
+        
 
 {- mergeTrees t1 t2
    PRECONS: Two Huffmantrees
    RETURNS: A Huffmantree with the sum of the weights and the two trees as it's children.
    EXAMPLE: 
+   ! UNTESTED !
 -}
 mergeTrees :: HuffmanTree -> HuffmanTree -> HuffmanTree
 mergeTrees t1 t2 = Node (weight t1 + weight t2) t1 t2
-  where weight :: HuffmanTree -> Int
-        weight (Leaf _ w)   = w
-        weight (Node w _ _) = w
+
+{- weight t
+   PRECONS: 
+   RETURNS:
+   EXAMPLE: 
+-}
+weight :: HuffmanTree -> Int
+weight (Leaf _ w)   = w
+weight (Node w _ _) = w
 
 {- codeTable h
    PRECONS: 
@@ -97,9 +124,6 @@ mergeTrees t1 t2 = Node (weight t1 + weight t2) t1 t2
  -}
 codeTable :: HuffmanTree -> Table Char BitCode
 codeTable = undefined
-
-
-
 
 {- encode h s
    PRECONS: All characters in s appear in h
